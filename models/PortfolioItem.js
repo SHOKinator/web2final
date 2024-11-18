d9234f4385d3740550e755cad77f7ff2c9575bf2
Shokay <148518400+SHOKinator@users.noreply.github.com>
2024-11-18 19:12:48 +0500

    Update PortfolioItem.js

--- models/PortfolioItem.js
+++ models/PortfolioItem.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const portfolioItemSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
-    images: [{ type: String }], // Ссылки на изображения
+    images: [{ type: String }], 
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
