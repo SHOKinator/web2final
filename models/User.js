0534e0e3359c75777f9531f6c5d64dc59cc47d90
Shokay <148518400+SHOKinator@users.noreply.github.com>
2024-11-18 18:59:03 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,16 +1,25 @@
-// models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  age: { type: Number, required: true },
-  gender: { type: String, required: true },
-  username: { type: String, unique: true, default: null }, // Сделать username необязательным
-  twoFASecret: { type: String },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' }, // Добавлено поле role
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    age: { type: Number, required: true },
+    gender: { type: String, required: true },
+    is2FAEnabled: { type: Boolean, default: false },
+    twoFactorSecret: { type: String },
+    role: { type: String, default: 'editor' },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+// Хэширование пароля перед сохранением
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
