804bf11e6bb872dd1087385797be2a8f70b58d59
Shokay <148518400+SHOKinator@users.noreply.github.com>
2024-11-18 18:59:45 +0500

    Update and rename auth.js to authRoutes.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware'); // Импортируем protect
+
+const router = express.Router();
+
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/setup-2fa', protect, authController.setup2FA);
+
+module.exports = router;
